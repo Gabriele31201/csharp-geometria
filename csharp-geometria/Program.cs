6e0228cca5f6e11383b44567d2efe128da62ac18
Gabriele31201 <gabri31201@gmail.com>
2023-04-17 16:11:32 +0200

    calculated perimeter

--- csharp-geometria/Program.cs
+++ csharp-geometria/Program.cs
@@ -9,4 +9,6 @@ Console.WriteLine();
 
 Rettangolo datiRettangolo = new Rettangolo (10, 20);
 
-Console.WriteLine("l' area del tuo rettangolo è:" + datiRettangolo.calcoloArea());
+Console.WriteLine("l' area del tuo rettangolo è: " + datiRettangolo.calcoloArea() + "cm2");
+
+Console.WriteLine("il perimetro del tuo rettangolo è: " + datiRettangolo.calcoloPerimetro() + "cm");
