daf99b62ea79bcdf3bdac39766a927ff25b28172
Gabriele31201 <gabri31201@gmail.com>
2023-04-17 16:09:02 +0200

    area calculated

--- csharp-geometria/Program.cs
+++ csharp-geometria/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+using ilMioRettangolo;
+
+Console.WriteLine(" CALCOLIAMO L'AREA E IL PERIMETRO DI UN RETTANGOLO");
+
+Console.WriteLine();
+
+
+
+Rettangolo datiRettangolo = new Rettangolo (10, 20);
+
+Console.WriteLine("l' area del tuo rettangolo è:" + datiRettangolo.calcoloArea());
--- csharp-geometria/rettangoli.cs
+++ csharp-geometria/rettangoli.cs
@@ -4,36 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class rettangolo
+namespace ilMioRettangolo
 {
-    //Attributi
-    public int baseRettangolo;
-    public int altezzaRettangolo;
 
 
-
-
-
-
-
-
-
-    //Metodi
-
-    public int calcoloArea()
+    public class Rettangolo
     {
-        int area = baseRettangolo * altezzaRettangolo;
-        return area;
+        //Attributi
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        //Costruttore
+
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+
+
+
+
+
+
+
+        //Metodi
+
+        public int calcoloArea()
+        {
+            int area = baseRettangolo * altezzaRettangolo;
+            return area;
+        }
+
+        public int calcoloPerimetro()
+        {
+            int perimetro = baseRettangolo * 2 + altezzaRettangolo * 2;
+            return perimetro;
+        }
     }
 
-    public int calcoloPerimetro()
-    {
-        int perimetro = baseRettangolo * 2 + altezzaRettangolo * 2;
-        return perimetro;
-    }
 }
-
-
 
 
 
